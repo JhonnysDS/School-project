9d487be9e3933298fe030779390226547dcf7c3c
JhonnysDS <98484070+JhonnysDS@users.noreply.github.com>
2023-04-16 16:03:02 -0500

    login hacho

---
+++ client/src/app/modules/material/material.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatSelectModule} from '@angular/material/select';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatListModule} from '@angular/material/list';
+import {MatTooltipModule} from '@angular/material/tooltip';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+
+  ],
+  exports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatRadioModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatCardModule,
+    MatTableModule,
+    MatDialogModule,
+    MatCheckboxModule,
+    MatProgressBarModule,
+    MatMenuModule,
+    MatSelectModule,
+    MatExpansionModule,
+    MatDatepickerModule,
+    MatListModule,
+    MatTooltipModule
+  ]
+})
+export class MaterialModule { }
